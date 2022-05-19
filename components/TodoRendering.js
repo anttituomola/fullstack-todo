011ed8a08df69df5ee68b1a622a5976fa5f3722e
Antti <anttituomola8@gmail.com>
2022-05-19 21:11:38 +0300

    show only user's own todos

--- components/TodoRendering.js
+++ components/TodoRendering.js
@@ -1,16 +1,25 @@
 import { useSelector, useDispatch } from "react-redux"
 import { todoActions } from "../features/todoSlice"
+import { useSession } from "next-auth/react"
 
 const TodoRendering = () => {
+  const { data: session, status } = useSession()
   const todos = useSelector(state => state.todos.todos)
   const dispatch = useDispatch()
 
-  const renderTodos = todos?.map(todo => {
+  let userId = "null"
+  if (session) {
+    userId = session.user.email
+  }
+
+  const personalTodos = todos.filter(todo => todo.userId === userId)
+
+  const renderTodos = personalTodos?.map(todo => {
     return (
       <div key={todo.id}>
         <p>
           {todo.text}
-          <input type="checkbox" checked={todo.completed} onChange={() => dispatch(todoActions.toggleTodo(todo.id))}/>
+          <input type="checkbox" checked={todo.completed} onChange={() => dispatch(todoActions.toggleTodo(todo.id))} />
           <button onClick={() => dispatch(todoActions.deleteTodo(todo.id))}>Delete</button>
         </p>
       </div>
